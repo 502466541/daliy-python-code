976b85607232c196e26049d7236dd78b91a55c43
Han Shaoqing <502466541@qq.com>
2020-09-04 18:58:27 +0800

    Update getGTF_gene_len_for_FPKM.py

--- getGTF_gene_len_for_FPKM.py
+++ getGTF_gene_len_for_FPKM.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python
+################################################################################################
+#   Usage:  python get_gene_len.py gencode.v31.annotation.gtf > gencode.v31.gene_length.txt    #
+################################################################################################
 import HTSeq
 import sys, getopt
 import os
@@ -6,7 +8,6 @@ import os
 annotation_file = sys.argv[1]
 merge_bedfile = annotation_file + '.merged.bed'
 def create_gene_len(annotation_file):
-	#print('Create '+annotation_file + '.length.txt file')
 	bedfile = annotation_file + '.unsort.bed'
 	sort_bedfile = annotation_file + '.sorted.bed'
 	merge_bedfile = annotation_file + '.merged.bed'
@@ -17,8 +18,7 @@ def create_gene_len(annotation_file):
 			if feature.type == 'gene':
 				number_gene+=1
 			if feature.type == 'exon':
-				row = [feature.attr['gene_id']+'_'+str(feature.iv.chrom),str(feature.iv.start),str(feature.iv.end),feature.attr['transcript_name'],feature.attr['gene_type'],str(feature.iv.strand)]
-
+				row = [feature.attr['gene_id']+'-'+str(feature.iv.chrom),str(feature.iv.start),str(feature.iv.end),feature.attr['transcript_name'],feature.attr['gene_type'],str(feature.iv.strand)]
 				cleanGTF.write('\t'.join(row) + '\n')
 		print ("total gene numbers: ",number_gene)
 	sort_command = 'sort -k1,1 -k2,2n '+ bedfile +' > ' + sort_bedfile
@@ -28,21 +28,19 @@ def create_gene_len(annotation_file):
 	os.system(merge_command)
 
 def cal_gene_len(merge_bedfile):
-
 	gene_len = dict()
 	with open(merge_bedfile) as f:
 		for line in f:
 			line = line.strip().split('\t')
 			name = str(line[0])
-
 			name_length = int(line[2])-int(line[1])
-
 			if name in gene_len:
 				gene_len[name] += name_length
 			else:
-				 gene_len[name] = name_length
-	for k,v in gene_len.items():
-		print(k.split('_')[0]+"\t"+str(v))
+				gene_len[name] = name_length
+		for k,v in gene_len.items():
+			
+			print(k.split('-')[0]+"\t"+str(v))
 
 
 create_gene_len(sys.argv[1])
